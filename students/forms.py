461962e3b42e76d341986b64ff3fe4a664c29015
Tapas Singhal <98687345+Shocker-lov-t@users.noreply.github.com>
2023-10-31 03:00:55 +0530

    Update forms.py

--- students/forms.py
+++ students/forms.py
@@ -10,6 +10,7 @@ class StudentForm(forms.ModelForm):
       'student_number': 'Student Number', 
       'first_name': 'First Name', 
       'last_name': 'Last Name', 
+      'email': 'Email',
       'field_of_study': 'Field of Study', 
       'gpa': 'GPA'
     }
@@ -17,6 +18,7 @@ class StudentForm(forms.ModelForm):
       'student_number': forms.NumberInput(attrs={'class': 'form-control'}), 
       'first_name': forms.TextInput(attrs={'class': 'form-control'}),
       'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+      'email': forms.EmailInput(attrs={'class': 'form-control'}),
       'field_of_study': forms.TextInput(attrs={'class': 'form-control'}),
       'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
     }
